1cc16af3f9dc7868ffbc78cad20f81b334a9a804
Baofeng Xue <xbaofeng@gmail.com>
2016-02-12 14:29:00 -0800

    r

--- example.bat
+++
@@ -1,4 +0,0 @@
-git pull
-call gradle fatJar
-copy build\libs\g-1.0.jar g.jar
-call java -jar g.jar -table lc$addr -cp z:/lc-common -mp z:/main -col city,zip,street,street_no -jpa LcAddress -username "Baofeng Xue" -ticket 66666
