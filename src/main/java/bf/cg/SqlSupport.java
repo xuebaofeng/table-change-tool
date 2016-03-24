b1a88042ebbd23133c2d1a658f3619b5cbf7c3d8
Baofeng Xue <xbaofeng@gmail.com>
2016-03-24 14:02:18 -0700

    r

--- src/main/java/bf/cg/SqlSupport.java
+++ src/main/java/bf/cg/SqlSupport.java
@@ -2,10 +2,7 @@ package bf.cg;
 
 import com.jolbox.bonecp.BoneCPConfig;
 import com.jolbox.bonecp.BoneCPDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.List;
@@ -15,65 +12,69 @@ import java.util.Map;
  * Created by Baofeng(Shawn) Xue on 12/23/15.
  */
 public class SqlSupport {
-	private static Logger logger = LoggerFactory.getLogger(SqlSupport.class);
 
-	private static BoneCPDataSource dataSource;
-	private static String env = "dev";
+    private static BoneCPDataSource dataSource;
 
-	static {
-		BoneCPConfig config = new BoneCPConfig();
+    static {
+        BoneCPConfig config = new BoneCPConfig();
 
-		dataSource.setDriverClass("oracle.jdbc.OracleDriver");
-	}
+        String env = "dev";
+        if ("dev".equals(env)) {
+            config.setJdbcUrl("jdbc:oracle:thin:@//lcdev.tlcinternal.com:1521/lcdev.lendingclub.com");
+        } else {
+            config.setJdbcUrl("jdbc:oracle:thin:@//lcscrubgg.tlcinternal.com:1521/LCSCRUBGG.lendingclub.com");
+        }
 
-	protected static DataSource getDataSource() {
-		return dataSource;
-	}
+        dataSource = new BoneCPDataSource(config);
 
-	protected static Connection getConnection() {
-		try {
-			return dataSource.getConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+        dataSource.setDriverClass("oracle.jdbc.OracleDriver");
+    }
 
-	protected static Map<Integer, Integer> getCalculateMap() {
-		Map<Integer, Integer> map = new HashMap<>();
-		List<String> lines = Main.readLines("EncryptedFieldSizeCalculator.csv");
-		for (String line : lines) {
-			String[] parts = line.split(",");
-			map.put(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
-		}
-		return map;
-	}
 
-	protected static Map<String, Integer> getSizeMap(String tableName) throws Exception {
-		GenTopLinkSql.logger.debug("table:{}", tableName);
+    protected static Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-		Map<String, Integer> sizeMap = new HashMap<>();
-		Connection connection = getConnection();
+    protected static Map<Integer, Integer> getCalculateMap() {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<String> lines = Main.readLines("EncryptedFieldSizeCalculator.csv");
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            map.put(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+        }
+        return map;
+    }
 
-		ResultSet rslt = connection.createStatement().executeQuery("SELECT * FROM " + tableName);
-		ResultSetMetaData rsmd = rslt.getMetaData();
-		int noCol = rsmd.getColumnCount();
-		String colName = "";
-		int colSize = 0;
+    protected static Map<String, Integer> getSizeMap(String tableName) throws Exception {
+        GenTopLinkSql.logger.debug("table:{}", tableName);
 
-		if (rslt.next()) {
+        Map<String, Integer> sizeMap = new HashMap<>();
+        Connection connection = getConnection();
 
-			for (int i = 1; i <= noCol; ++i) {
-				colName = rsmd.getColumnName(i);
-				colSize = rsmd.getColumnDisplaySize(i);
-				int columnType = rsmd.getColumnType(i);
-				logger.debug("name:{},type:{},size:{}", colName, columnType, colSize);
-				if (Types.DATE == columnType || Types.TIMESTAMP == columnType) {
-					sizeMap.put(colName, 48);
-				} else sizeMap.put(colName, colSize);
-			}
-		}
-		connection.close();
-		return sizeMap;
-	}
+        assert connection != null;
+        ResultSet rslt = connection.createStatement().executeQuery("SELECT * FROM " + tableName);
+        ResultSetMetaData rsmd = rslt.getMetaData();
+        int noCol = rsmd.getColumnCount();
+        String colName;
+        int colSize;
+
+        if (rslt.next()) {
+
+            for (int i = 1; i <= noCol; ++i) {
+                colName = rsmd.getColumnName(i);
+                colSize = rsmd.getColumnDisplaySize(i);
+                int columnType = rsmd.getColumnType(i);
+                if (Types.DATE == columnType || Types.TIMESTAMP == columnType) {
+                    sizeMap.put(colName, -1);
+                } else sizeMap.put(colName, colSize);
+            }
+        }
+        connection.close();
+        return sizeMap;
+    }
 }
